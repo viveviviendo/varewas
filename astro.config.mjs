cbc6882e23e33defe5829da96ddcdb88bc8aa7d8
viveviviendo <mariano@solarxquare.com>
2026-01-04 17:45:09 +0100

    3
    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en", "es", "fr"],
-        routing: {
-            prefixDefaultLocale: true
-        }
-    }
 });
